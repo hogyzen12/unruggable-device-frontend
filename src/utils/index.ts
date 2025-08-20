1b58f603b3a332eaebedf4510df285c58ce34f60
hogyzen12 <billypapas12@gmail.com>
2025-08-20 18:20:09 +0200

    resolving clashes for deployment and building

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,7 +8,7 @@ import highlightFirstmv from "/assets/videos/highlight-first.mp4";
 import highlightSectmv from "/assets/videos/hightlight-third.mp4";
 import highlightThirdmv from "/assets/videos/hightlight-sec.mp4";
 import highlightFourthmv from "/assets/videos/hightlight-fourth.mp4";
-import exploremv from "/assets/videos/explore.mp4";
+//import exploremv from "/assets/videos/explore.mp4";
 import framemv from "/assets/videos/frame.mp4";
 
 import apple from "/assets/images/apple.svg";
@@ -35,7 +35,7 @@ export const highlightFirstVideo = highlightFirstmv;
 export const highlightSecondVideo = highlightSectmv;
 export const highlightThirdVideo = highlightThirdmv;
 export const highlightFourthVideo = highlightFourthmv;
-export const exploreVideo = exploremv;
+//export const exploreVideo = exploremv;
 export const frameVideo = framemv;
 
 export const appleImg = apple;
